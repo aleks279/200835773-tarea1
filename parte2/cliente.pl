4ac0013efa131a947d533d48ddecdf87c71097d9
Saul Zamora <szamora@pernix-solutions.com>
2017-03-01 19:32:58 -0600

    add console command options. refactor query

--- parte2/cliente.pl
+++ parte2/cliente.pl
@@ -1,38 +1,74 @@
 use strict;
-use Getopt::Long;
+use Getopt::Std;
 use CGI::Carp qw(fatalsToBrowser);
 use LWP::UserAgent;
 use HTTP::Request;
 use HTML::TableExtract;
 
-my $url = "https://www.wikileaks.org/hackingteam/emails/";
-my $query = "?q=&mfrom=%40\@gmail.com&mto=&title=&notitle=&date=&nofrom=&noto=&count=10000&sort=0#searchresult";
+my %options=();
+getopts("t:", \%options);
 
-my $userAgent = LWP::UserAgent->new;
-$userAgent->agent("Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Mobile Safari/537.36");
+my $query_params = "";
 
-my $request = HTTP::Request->new(POST => $url);
-$request->content_type('application/x-www-form-urlencoded');
-$request->content($query);
-
-my $response = $userAgent->request($request);
-my $content = $response->content();
-
-my $te = HTML::TableExtract->new( headers => [qw(Date From To)]);
-
-$te->parse($content);
-my $table = $te->tables;
-
-my $ts = $te->tables;
-
-foreach $ts ($te->tables) {
-  my $row = $ts->rows;
-
-  open(my $output_file, '>', 'results.csv') or die 'Shit';
-  print $output_file "Date,From,To\n";
-  foreach $row ($ts->rows) {
-    print $output_file join(',',@$row), "\n";
+if (defined $options{t}) {
+  if ( $options{t} eq "email_from" ) {
+    $query_params = "?q=&mfrom=%40@ARGV&mto=&title=&notitle=&date=&nofrom=&noto=&count=10000&sort=0#searchresult";
+    makeRequest();
+  } elsif ( $options{t} eq "email_to" ) {
+    $query_params = "?q=&mfrom=&mto=%40@ARGV&title=&notitle=&date=&nofrom=&noto=&count=10000&sort=0#searchresult";
+    makeRequest();
+  } elsif ( $options{t} eq "email_content") {
+    my $text = join('+', @ARGV);
+    $query_params = "?q=$text&mfrom=&mto=&title=&notitle=&date=&nofrom=&noto=&count=10000&sort=0#searchresult";
+    print $query_params, "\n";
+    makeRequest();
+  } else {
+    printOptions();
   }
-  close $output_file;
-  print "Finished!\n";
+}
+
+sub makeRequest{
+  my $url = "https://www.wikileaks.org/hackingteam/emails/";
+
+  my $userAgent = LWP::UserAgent->new;
+  $userAgent->agent("Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Mobile Safari/537.36");
+
+  my $request = HTTP::Request->new(POST => $url);
+  $request->content_type('application/x-www-form-urlencoded');
+  $request->content($query_params);
+
+  my $response = $userAgent->request($request);
+  my $content = $response->content();
+
+  my $te = HTML::TableExtract->new( headers => [qw(Date From To)]);
+
+  $te->parse($content);
+  my $table = $te->tables;
+
+  my $ts = $te->tables;
+
+  foreach $ts ($te->tables) {
+    my $row = $ts->rows;
+
+    open(my $output_file, '>', 'results.csv') or die 'Shit';
+    print $output_file "Date,From,To\n";
+    foreach $row ($ts->rows) {
+      print $output_file join(',',@$row), "\n";
+    }
+    close $output_file;
+    print "Finished!\n";
+  }
+}
+
+sub printOptions{
+  print "ITCR - Ing Computacion\n";
+  print "Bases de Datos II\n";
+  print "Prof. Kevin Moraga\n";
+  print "Tarea 1 by Saul Zamora 200835773\n";
+  print "Uso:\n";
+  print "perl cliente.pl -t <tipo-consulta> <texto>\n";
+  print "<tipo-campo>:\n";
+  print "\t email_from : remitente del email\n";
+  print "\t email_to : destinatario del email\n";
+  print "\t email_content : palabra(s) a buscar en el cuerpo del email\n\n";
 }
